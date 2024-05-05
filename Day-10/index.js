a169750eed94d83668c1fd3a8109831f033a0419
MehfoozAbbas110 <33392056+MehfoozAbbas110@users.noreply.github.com>
2024-05-05 12:15:30 +0530

    Day10

---
+++ Day-10/index.js
@@ -0,0 +1,28 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"]; 
+fruits.pop();
+console.log(fruits);
+
+
+const fruit = ["Banana", "Orange", "Apple", "Mango"];
+fruit.push("Kiwi");
+console.log(fruit);
+
+
+const car = ["BMW", "Audi", "Tesla", "Ford"];
+car.sort();
+console.log(car);
+
+
+const sname = ["Mehfooz", "Abbas", "Hasni", "Ibrahim"];
+sname.splice(2, 0, "Abhishek", "Shukla");
+console.log(sname);
+
+const bike = ["Honda", "Hero", "Bajaj", "Yamaha"];
+bike.shift();
+console.log(bike);
+
+const city = ["Lucknow", "Kanpur", "Mumbai", "Delhi"];
+city.unshift("Pune");
+console.log(city);
+
+
